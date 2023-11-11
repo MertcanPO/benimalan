06f775f5b19fb00d8d2d4ffab7f386e9e87ea282
MertcanPO <88785328+MertcanPO@users.noreply.github.com>
2023-11-12 00:12:40 +0300

    basic try

---
+++ deneme1.py
@@ -0,0 +1,10 @@
+programlama_dilleri = ["python","php",'java','c']
+
+def toplama(a,b,c):
+
+    """Bu fonksiyon üç adet değeri döndürü.r"""
+    return a + b + c
+def selamla(isim):
+    """Bu fonksiyon insanları selamlar."""
+    print('selam',isim)
+
---
+++ modül1.py
@@ -0,0 +1,7 @@
+import deneme1
+
+print(deneme1.toplama(6,7,8))
+
+deneme1.selamla('Mertcan')
+
+print(deneme1.programlama_dilleri)
